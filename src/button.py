c48371ae76b552f3a40b771e3549423167c1600b
Abel Lucas <abelucas009@gmail.com>
2025-08-15 16:54:36 -0300

    Update button.py

--- src/button.py
+++ src/button.py
@@ -2,5 +2,5 @@ from customtkinter import CTkButton
 
 class Button(CTkButton):
     def __init__(self, master, txt, img, **kwargs):
-        super().__init__(master, text=txt, image= img, anchor="nsew",**kwargs)
+        super().__init__(master, text=txt, image= img, compound="top", anchor="nsew",**kwargs)
         self.configure(height=90, anchor="center")
